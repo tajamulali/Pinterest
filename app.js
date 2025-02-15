c2358ea57e9c2f12f327dbbbc11881cb87bb168c
tajamulali <nuwaib14@gmail.com>
2025-02-15 23:18:49 +0530

    nnnn

--- app.js
+++ app.js
@@ -10,15 +10,11 @@ var usersRouter = require('./routes/users');
 const passport = require('passport');
 
 const mongoose = require('mongoose');
-require('dotenv').config();  // Ensure environment variables are loaded
+require('dotenv').config(); 
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-}).then(() => {
-  console.log("✅ Connected to MongoDB Atlas");
-}).catch(err => {
-  console.error("❌ MongoDB Connection Error:", err);
 });
 
 var app = express();
