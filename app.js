5dc21479b7a2f2c30ba40a9633e7aa07d9661ee9
tajamulali <nuwaib14@gmail.com>
2025-02-16 00:30:45 +0530

    lovvvvvv

--- app.js
+++ app.js
@@ -13,15 +13,20 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 require('dotenv').config(); 
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('MongoDB connection error:', err));
+
+mongoose.connect(process.env.MONGO_URI);
 
 
 var app = express();
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;  // Use Render's PORT or default to 3000
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 
 app.use(session({
   secret: 'pinterest',
