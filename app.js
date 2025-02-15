48d5b5e37f5ebdd8ff0fa170e1a8b40d2f191993
tajamulali <nuwaib14@gmail.com>
2025-02-16 00:38:25 +0530

    buuuu

--- app.js
+++ app.js
@@ -18,8 +18,6 @@ mongoose.connect(process.env.MONGO_URI);
 
 
 var app = express();
-const app = express();
-
 const PORT = process.env.PORT || 3000;  // Use Render's PORT or default to 3000
 
 app.listen(PORT, () => {
